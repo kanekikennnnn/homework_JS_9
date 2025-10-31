9313e3a79b6b09882d7bff493171db80acc86d40
David <qocharyandavit493@gmail.com>
2025-10-31 14:56:45 +0400

    v1.2

--- js/script.js
+++ js/script.js
@@ -2,52 +2,77 @@
 
 // 1. Տրված է թիվ. Տպեք այս թվի առաջին նիշը console - ում: 512
 
-// function first(num){
-//     let str = '' + num;
-//     return str[0]
+// let num = 652;
+
+// while(true){
+//     if(num < 10){
+//         num = Math.floor(num);
+//         break
+//     }
+//     num /= 10
 // }
 
-// console.log(first(284));
+// console.log(num);
 
 
 // 2. Տրված է թիվ. Տպեք այս թվի վերջին նիշը console - ում: 512
 
-// function last(num){
-//     let str = '' + num;
-//     return str[str.length - 1]
-// }
+// let num = 512583;
+// num /= 10;
+// num = (num - parseInt(num)) * 10;
+// num = parseInt(num);
+// console.log(num);
 
-// console.log(last(284));
+
 
 // 3. Տրված է թիվ. Տպեք այս թվի առաջին և վերջին թվանշանների գումարը console - ում: 512 5+2
 
-// function foo(num){
-//     let str = '' + num;
-//     return +str[0] + +str[str.length - 1]
+// let num = 5124;
+// let n1 = num;
+// let n2 = num;
+
+// while(true){
+//     if(n1 < 10){
+//         n1 = Math.floor(n1);
+//         break
+//     }
+//     n1 /= 10
 // }
 
-// console.log(foo(284));
+// n2 /= 10;
+// n2 = (n2 - parseInt(n2)) * 10;
+// n2 = Math.round(n2);
+
+// console.log(n1 + n2);
+
 
 // 4. Տրված է թիվ. Տպեք այս թվի թվանշանների քանակը: 512 => 3
 
-// function foo(num){
-//     let str = '' + num;
-//     return str.length
+// let num = 511582;
+// let count = 0;
+
+// while(true){
+//     count++
+//     num /= 10;
+//     if(num < 1){
+//         break
+//     }
 // }
 
-// console.log(foo(284));
+// console.log(count);
+
 
 // 5. Տրված է երկու ամբողջ թիվ։ Ստուգեք, որ առաջին թիվը բաժանվում է երկրորդի վրա առանց մնացորդի:
 
-function foo(n1,n2){
-    if(n1 % n2){
-        return false
-    }
+// function foo(n1,n2){
+//     if(n1 % n2){
+//         return false
+//     }
 
-    return true
-}
+//     return true
+// }
 
-console.log(foo(40,50));
+// console.log(foo(40,50));
 
 
 
